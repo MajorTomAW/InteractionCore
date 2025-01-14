802be17b884d7510a34cb940b0af8108953fe313
MajorTomAW <tom.wrnr.private@gmail.com>
2025-01-14 14:52:44 +0100

    Initial Commit

---
+++ Source/InteractionCore/Private/InteractionCore.cpp
@@ -0,0 +1,17 @@
+﻿#include "InteractionCore.h"
+
+#define LOCTEXT_NAMESPACE "FInteractionCoreModule"
+
+void FInteractionCoreModule::StartupModule()
+{
+    
+}
+
+void FInteractionCoreModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FInteractionCoreModule, InteractionCore)
